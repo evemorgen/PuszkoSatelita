d2512ac1309114d3137e0b1293f4eef318eaf4ea
Tymoteusz Chmielecki <typho0s@icloud.com>
2016-11-06 20:17:44 +0100

    Start the cansat code

---
+++ code/cansat_code/main.py
@@ -0,0 +1,15 @@
+"""
+JJ CanSat team code
+Licensed under h3h3 license
+"""
+
+from time import sleep
+
+def main():
+    while True:
+        print("JJ CanSat best CanSat team")
+        sleep(10)
+
+
+if __name__ = '__main__':
+    main()
