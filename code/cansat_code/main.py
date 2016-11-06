10b0688a0f08eef5ea31ba6035da37463fc8abff
Tymoteusz Chmielecki <typho0s@icloud.com>
2016-11-06 21:37:23 +0100

    Corrige = to == on 14 line

--- code/cansat_code/main.py
+++ code/cansat_code/main.py
@@ -5,11 +5,12 @@ Licensed under h3h3 license
 
 from time import sleep
 
+
 def main():
     while True:
         print("JJ CanSat best CanSat team")
         sleep(10)
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
