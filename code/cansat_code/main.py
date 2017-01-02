8459f25408f554ae1a3382aaa0c7b9520a288499
Tymoteusz Chmielecki <typho0s@icloud.com>
2017-01-02 09:06:18 +0100

    some code changes i guess

--- code/cansat_code/main.py
+++ code/cansat_code/main.py
@@ -6,6 +6,7 @@ Licensed under h3h3 license
 from time import sleep
 from thermometer import temperature_sensor as temp
 
+
 def main():
     temp_sensor = temp.TemperatureSensor(1)
     temp_sensor.loop()
