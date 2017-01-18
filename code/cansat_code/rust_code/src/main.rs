d9a6e71beeeed7d957d9ac056dca9a16f0310624
Tymoteusz Chmielecki <typho0s@icloud.com>
2017-01-18 19:07:51 +0100

    Test case

---
+++ code/cansat_code/rust_code/src/main.rs
@@ -0,0 +1,23 @@
+extern crate i2cdev;
+
+use std::thread;
+use std::time::Duration;
+
+use i2cdev::core::*;
+use i2cdev::linux::{LinuxI2CError, LinuxI2CDevice};
+
+const ADAFRUIT_THERMOMETER_ADDR: u8 = 0x44;
+
+fn read_temp() -> Result<(), LinuxI2CError> {
+    let mut dev = try!(LinuxI2CDevice::new("/dev/i2c-0", ADAFRUIT_THERMOMETER_ADDR));
+
+    loop {
+        let mut buf: [u8; 6] = [0; 6];
+
+        dev.smbus_write_byte(0x00).unwrap();
+        thread::sleep(Duration::from_millis(10));
+        dev.read(&mut buf).unwrap();
+        println!("Reading: {:?}", buf);
+        thread::sleep(Duration::from_millis(1000));   
+    }
+}
