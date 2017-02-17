82f60c2f020f05409570ff5a3db807a858b1ad06
Tymoteusz Chmielecki <typho0s@icloud.com>
2017-02-17 11:44:26 +0100

    Change the test file code

--- code/cansat_code/accelerometer/accel_sensor.py
+++ code/cansat_code/accelerometer/accel_sensor.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+from datetime import datetime
 from altimu import AltIMU
 
 imu = AltIMU()
@@ -7,8 +7,20 @@ imu.enable()
 
 imu.calibrateGyroAngles()
 
+#for x in range(1000):
+#    startTime = datetime.now()
+#    angles = imu.trackGyroAngles(deltaT = 0.0002)
+
+#print angles
+
+start = datetime.now()
 
 while True:
+    stop = datetime.now() - start
+    start = datetime.now()
+    deltaT = stop.microseconds/1000000.0
+    print(" ")
+    print("Loop:", deltaT)
     print("Accel:", imu.getAccelerometerAngles())
     print("Gyro:", imu.trackGyroAngles(deltaT = deltaT))
     sleep(0.5)
