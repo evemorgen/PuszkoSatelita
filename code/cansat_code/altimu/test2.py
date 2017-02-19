2924e8fa7a553f6856e526e0e8c5adf6282d7ddc
Jan Dziedzic <jandziedzic99@gmail.com>
2017-02-19 16:55:53 +0000

    First tests of IMU using nice library found on github. test2.py works, gotta figure out data meaning.

---
+++ code/cansat_code/altimu/test2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from datetime import datetime
+from time import sleep
+
+from altimu import AltIMU
+
+imu = AltIMU()
+imu.enable()
+
+imu.calibrateGyroAngles()
+
+#for x in range(1000):
+#    startTime = datetime.now()
+#    angles = imu.trackGyroAngles(deltaT = 0.0002)
+
+#print angles
+
+start = datetime.now()
+
+#while True:
+stop = datetime.now() - start
+start = datetime.now()
+deltaT = stop.microseconds/1000000.0
+#    print " "
+#    print "Loop:", deltaT
+print "Accel:", imu.getAccelerometerAngles()
+print "Gyro:", imu.getGyroRotationRates()
+#    sleep(0.02)
