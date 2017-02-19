0bfb73dc3d693a4598cb48f0a84270df8ddecd02
Jan Dziedzic <jandziedzic99@gmail.com>
2017-02-19 13:13:23 +0000

    Added humidity reading based on code for thermometer. Changes required: implement python daemon, because calling python script from bash every iteration is slow.

--- code/cansat_code/thermometer_humidity_sensor/SHT31d.sh
+++ code/cansat_code/thermometer_humidity_sensor/SHT31d.sh
@@ -4,6 +4,10 @@ do
 readnumber=$(($(more ~/data/temperature/last_read_number.txt )+1))
 echo $readnumber > ~/data/temperature/last_read_number.txt
 python SHT31_temperature_reader.py >> ~/data/temperature/$readnumber .txt
-echo $readnumber,$(date) > ~/data/temperature/datatable.txt
+echo $readnumber,$(date) >> ~/data/temperature/datatable.txt
+readnumber=$(($(more ~/data/humidity/last_read_number.txt )+1))
+echo $readnumber > ~/data/humidity/last_read_number.txt
+python SHT31_humidity_reader.py >> ~/data/humidity/$readnumber .txt
+echo $readnumber,$(date) >> ~/data/humidity/datatable.txt
 sleep 1
 done
