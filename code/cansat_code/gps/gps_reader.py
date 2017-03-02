3da7dd10d7fed21f2dd6ee2f101db1291eae7103
Jan Dziedzic <jandziedzic99@gmail.com>
2017-03-02 02:30:24 +0100

    fixed bug causing file to stay open for too long

--- code/cansat_code/gps/gps_reader.py
+++ code/cansat_code/gps/gps_reader.py
@@ -9,8 +9,10 @@ while True:
         read = last_read + 1
         file_last_read.write(str(read))
         file_last_read.close()
+        data = str(read_gps_data())
         file_output = open('/home/pi/data/gps/'+str(read), 'w')
-        file_output.write(str(read_gps_data()))
+        file_output.write(data)
+        file_output.close()
         file_datetable.write(str(read)+','+datetime.now().strftime('%Y,%m,%d,%H,%M,%S')+'\n')
 ###     uncomment next line for less data flow  ###
         time.sleep(1)
