98930ce41c3788e853d073d23d34dc28e6897e4f
Jan Dziedzic <jandziedzic99@gmail.com>
2017-03-07 00:18:22 +0100

    created starting script

---
+++ code/cansat_code/starter/magnitudo_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+daemon -r ~/PuszkoSatelita/code/cansat_code/gps/gps_reader.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/altimu/altimu_reader.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/thermometer_humidity_sensor/SHT31_humidity_reader.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/thermometer_humidity_sensor/SHT31_temperature_reader.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/aggregator/aggregator.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/radio/radio_transmitter.py
+
+echo Magnitudo started!
+
