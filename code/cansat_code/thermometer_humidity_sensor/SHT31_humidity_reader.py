4e05cc54dec6e8dea75dc27a9dc07f6aa25a850d
Jan Dziedzic <jandziedzic99@gmail.com>
2017-03-07 01:46:11 +0100

    improved overall stability with file closing

--- code/cansat_code/thermometer_humidity_sensor/SHT31_humidity_reader.py
+++ code/cansat_code/thermometer_humidity_sensor/SHT31_humidity_reader.py
@@ -6,12 +6,15 @@ import time
 file_datetable = open('/home/pi/data/humidity/datetable.txt', 'a')
 while True:
         file_last_read = open('/home/pi/data/humidity/last_read_number.txt', 'r+')
-        last_read=int(file_last_read.read())
+        last_read = int(file_last_read.read())
+        file_last_read.close()
         read=last_read+1
         file_output = open('/home/pi/data/humidity/'+str(read), 'w')
         file_output.write(str(read_humidity()))
+        file_output.close()
         file_last_read = open('/home/pi/data/humidity/last_read_number.txt', 'w')
         file_last_read.write(str(read))
+        file_last_read.close()
         file_datetable.write(str(read)+','+datetime.now().strftime('%Y,%m,%d,%H,%M,%S')+'\n')
 ###     uncomment next line for less data flow  ###
-       time.sleep(1)
+        time.sleep(1)
