e3473f32ea618ebf8101b7c93ca142b7616b70b2
Jan Dziedzic <jandziedzic99@gmail.com>
2017-03-22 01:56:58 +0100

    Readers for 2 batts

--- code/cansat_code/voltage/voltage.py
+++ code/cansat_code/voltage/voltage.py
@@ -2,7 +2,7 @@ import Adafruit_ADS1x15
 adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
 DATARATE=128
-def read_voltage(batnum)
+def read_voltage(batnum):
     value = adc.read_adc(batnum, gain=GAIN, data_rate=DATARATE)
     return value
 
