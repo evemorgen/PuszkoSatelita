62de390e5d6547e2f7670a444a1d2a98c6888c3c
Jan Dziedzic <jandziedzic99@gmail.com>
2017-03-23 22:00:41 +0100

    added new sensors based on ADC

--- code/cansat_code/starter/magnitudo_start.sh
+++ code/cansat_code/starter/magnitudo_start.sh
@@ -4,6 +4,9 @@ daemon -r ~/PuszkoSatelita/code/cansat_code/gps/gps_reader.py
 daemon -r ~/PuszkoSatelita/code/cansat_code/altimu/altimu_reader.py
 daemon -r ~/PuszkoSatelita/code/cansat_code/thermometer_humidity_sensor/SHT31_humidity_reader.py
 daemon -r ~/PuszkoSatelita/code/cansat_code/thermometer_humidity_sensor/SHT31_temperature_reader.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/voltage/voltage_reader0.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/voltage/voltage_reader1.py
+daemon -r ~/PuszkoSatelita/code/cansat_code/wind/wind_reader.py
 #having next line commented may decrease stability
 sleep 2
 daemon -r ~/PuszkoSatelita/code/cansat_code/aggregator/aggregator.py
