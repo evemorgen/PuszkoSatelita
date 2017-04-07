da4975e585abd16de2bfc49977c66c1ec4ae2c30
JJ on RPI <jj@rpi>
2017-04-07 18:39:26 +0000

    +x, add missing file

---
+++ code/cansat_code/altimu/read_altimu.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from time import sleep
+
+from lsm6ds33 import LSM6DS33
+from lis3mdl import LIS3MDL
+from lps25h import LPS25H
+
+imu = LSM6DS33()
+imu.enableLSM()
+
+magnet = LIS3MDL()
+magnet.enableLIS()
+
+baro = LPS25H()
+baro.enableLPS()
+###Uncomment below lines to show debug###
+"""
+while True:
+    print "Gyro:", imu.getGyroscopeRaw()
+    print "Accelerometer:", imu.getAccelerometerRaw()
+    print "Magnet:", magnet.getMagnetometerRaw()
+    print "hPa:", baro.getBarometerMillibars()
+    print "Altitude:", baro.getAltitude()
+    sleep(5)
+"""
+
+
+def read_altimu():
+    # type: () -> object
+    barolist = []
+    list.append(barolist, baro.getBarometerMillibars())
+    list.append(barolist, baro.getAltitude())
+    string = ",".join(map(str, imu.getGyroscopeRaw() +
+                          imu.getAccelerometerRaw() +
+                          magnet.getMagnetometerRaw() +
+                          barolist
+                          ))
+    return string
